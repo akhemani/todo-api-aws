58b567f2ead3ddb1e82e0c8e6b36060a073136ac
Ameet Khemani <ameetkhemani@gmail.com>
2025-09-28 08:09:22 +0530

    Delete src directory

--- src/main/java/com/todoapp/todoapi/repo/TodoRepository.java
+++
@@ -1,8 +0,0 @@
-package com.todoapp.todoapi.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.todoapp.todoapi.model.Todo;
-
-public interface TodoRepository extends JpaRepository<Todo, Long> {
-}
